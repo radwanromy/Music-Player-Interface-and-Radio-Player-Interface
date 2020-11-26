ab29282d094e927eee00c652f678ae5bb9603275
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2020-11-27 00:10:43 +0600

    Test Interface 3 Final Commit

--- Phone.cs
+++ Phone.cs
@@ -6,9 +6,22 @@ namespace Test_Interface_3
 {
     class Phone : MusicFile , MusicPlayerInterface
     {
-        public Phone() { }
+        private MusicFile[] musicFiles;
+        private int musicFileCount;
+        public int MusicFileCount
+        {
+            get { return musicFileCount; }
+            set { musicFileCount = value; }
+        }
+        public Phone() 
+        {
+            musicFiles = new MusicFile[500];
+            musicFileCount = 0;
+        }
         public Phone(string title, string artist, int yearOfRelease, int durationInSeconds) : base( title,  artist, yearOfRelease, durationInSeconds)
         {
+            musicFiles = new MusicFile[500];
+            musicFileCount = 0;
 
         }
         public void switchs( bool currentMode)
@@ -50,19 +63,54 @@ namespace Test_Interface_3
                 Console.WriteLine("The Music Player Music Loudness Level In Negative Level That Why " + loudnessLevel + "Considered As 0 Level Loudness.");
             }
         }
-
+        public void retune(double frequencyLevel)
+        {
+            Console.WriteLine("The Radio Player Cuurent Frequency Number : " + frequencyLevel);
+        }
+        public void AddMusicFile(params MusicFile[] musicFiles)
+        {
+            foreach (var a in musicFiles)
+            {
+                if (musicFileCount < 500)
+                {
+                    this.musicFiles[musicFileCount++] = a;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot Create Account:  " + a.Title);
+                }
+            }
+        }
+        public void ShowAllMusicFile()
+        {
+            for (int i = 0; i < musicFileCount; i++)
+            {
+                this.musicFiles[i].ShowInfo();
+                
+            }
+        }
         public void playNext()
         {
             Console.WriteLine("Next Music In The PlayList Of The MusicPlayer.");
+            for (int i = 1; i < musicFileCount; i++)
+            {
+                this.musicFiles[i+1].ShowInfo();
+                break;
+            }
         }
 
         public void playPrevious()
         {
             Console.WriteLine("Previous Music In The PlayList Of The MusicPlayer.");
+            for (int i = 0; i < musicFileCount; i++)
+            {
+                this.musicFiles[i].ShowInfo();
+                break;
+            }
         }
 
         public void changeChannel()
-    {
+        {
             Console.WriteLine("Changing The Radio Channel.");
         }
 
--- Program.cs
+++ Program.cs
@@ -6,11 +6,40 @@ namespace Test_Interface_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+            Console.WriteLine("|||||||||||||||||||||||Music About||||||||||||||||||||||||||");
+            
             Phone m1 = new Phone("It's the Most Wonderful Time of the Year", "Andy Williams", 2020, 300);
             m1.ShowInfo();
+            Console.WriteLine();
+            Console.WriteLine("|||||||||||||||||||||||Change Title|||||||||||||||||||||||||");
             m1.changeTitle("It is the Most Wonderful Time of the Year");
             m1.ShowInfo();
+            Console.WriteLine();
+            Console.WriteLine("||||||||||||||||||||||Swich On Call|||||||||||||||||||||||||");
+            m1.switchs(true);
+            Console.WriteLine();
+            Console.WriteLine("|||||||||||||||||||Music Play On Call ||||||||||||||||||||||");
+            m1.play(true);
+            Console.WriteLine();
+            Console.WriteLine("||||||||||||||Set Volume Call with Loudness 5|||||||||||||||");
+            m1.setVolume(5);
+            Phone m2 = new Phone("Jingle Bell Rock", "Bobby Helms", 2020, 300);
+            Phone m3 = new Phone("Holly Jolly Christmas", "Burl Ives", 2020, 300);
+            Phone m4 = new Phone("Memories", "Maroon 5", 2020, 300);
+            Phone list = new Phone();
+            list.AddMusicFile(m2, m1, m3, m4);
+            Console.WriteLine();
+            Console.WriteLine("||||||||||||||||||||||Next Music Info|||||||||||||||||||||||");
+            list.playNext();
+            Console.WriteLine();
+            Console.WriteLine("||||||||||||||||||||Previous Music Info|||||||||||||||||||||");
+            list.playPrevious();
+            Console.WriteLine();
+            Console.WriteLine("||||||||||||||||||||||||Music List||||||||||||||||||||||||||");
+            
+            
+            list.ShowAllMusicFile();
+            
         }
     }
 }
