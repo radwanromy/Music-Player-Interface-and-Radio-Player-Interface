bb34fb1f5cfa88f190cbf0120665e87be517dd48
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2020-11-26 06:09:06 +0600

    Test Interface 3 Commit Number 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Interface_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
